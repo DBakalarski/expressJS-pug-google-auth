5a60dda6098517e5f9734c12a1752cc8eb058fe0
Dawid Bakalarski <dawid.bakalarski@gmail.com>
2018-07-16 22:10:43 +0200

    Excercise 17.7

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID' : '749325367094-81nj4q9i3k86p9vcvhn8ca1hjljg50j4.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET' : 'O--pz4vLB8JLYjsM3LNYt3ix',
+	'CALLBACK_URL' : 'http://localhost:3000/auth/google/callback'
+};
